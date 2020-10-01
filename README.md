615781047b2b91793bb4e5df8e669be3a5c9e292
Benjamin Dionysus <benjamin@sixbynine.com>
2020-10-01 10:57:38 -0500

    Cleaned up the php code and removed my password from it! Although I guess github still has the older versions, ah well. Also fleshed out the readme.md

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-*Exciting Quiz Machine!*
+# Exciting Quiz Machine!
+
+Fully functional version at this URI: https://b-dionysus.github.io/homework4-quiz/
+
+# Description
+What an exciting quiz game! First we check to see if someone on this computer has attempted the quiz already. If they have, we'll tell them their previous high score in an attempt to motivate them to do better this time!
+
+Then we present them with a question, and some possible answers (in this case all of the quiz questions have four possible answers, but there's no reason they couldn't have more, or fewer). An ominous timer ticks down in the corner. If the user get a question correct, they get a point and a new question. If not, the timer is reduced and they get an incorrect mark and a new question.
+
+The quiz continues until the user has answered every question correctly (they get as many chances to answer them as they want) or until they finally run out of time. At that point, they see their final score (time let plus right answers, minus wrong answers). They enter their name (prefilled, if they have tried the game previously) and then see an overall scoreboard, the the highest scores from everyone who has ever played the game, throughout the world!! Technically, this is just as competitive as the World Series.
+
+
+# Bugs / ToDo
+The user is asked to enter their name, and I tried my best to sanitize the user input. I haven't been able to hack it yet, but I worry that I've left myself open to some sort of exploit or other. So the remaining ToDo would be to get someone more security knowledgable than I am to audit the php code.
--- highScore-PublicCode.php
+++ highScore-PublicCode.php
@@ -1,30 +1,49 @@
 <?php
+// I learned so many exciting things about COR restrictions!
+// I still have idea how a solution as simple as adding the following
+// line of code will grant my script access, while still defeating the
+// bad guys, but it works!
 header("Access-Control-Allow-Origin: *");
 
-$mysqli = new mysqli("localhost", "damien", "fhben", "damiendb");
+// First we set up a connection to the database. I am hoping that I remember to scrub my
+// password from the public version of this file! The real php code lives privately on my
+// server, safe from prying eyes.
+$mysqli = new mysqli("localhost", "#######", "#######", "########");
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
+
+
+// Next we check to see if we are receiving a username and a score. If we are, I hope to heck
+// that real_escape_string() will prevent any nefarious hax0rs from exploiting my database.
+// My code is the one that sent score, so we can trust that it is just a number
+// (We definitely cannot trust this--anyone can navigate to the same URL and input anything they
+// want in here. But hopefully mysqli->prepare() will save me, in that case, because it will insist on
+// getting a number. It's still quite possible to cheat this way, though, and put yourself on the scoreboard 
+// with a ridulously high score!)
 if(isset($_REQUEST['user'])) $name=$mysqli->real_escape_string($_REQUEST['user']); 
 else $name="NULL";
 if(isset($_REQUEST['score'])) $score=$_REQUEST['score']; 
 else $pass="NULL";
 
+
+// We prepare, by telling mysql what to expect
 if(!is_null($name) && !is_null($score)){
     $addScorePrep=$mysqli->prepare("INSERT INTO highScore_codeBootcamp (name, score) VALUES (?, ?)");
     if ( false===$addScorePrep)
     die('prepare() failed: ' . htmlspecialchars($mysqli->error));
 
+
+    // And we insist on a string and a number here. This prints the error if the
+    // bind fails, which is useful in testing / development. In the real world, of course,
+    // there's no way for anyone to see this error and so it just fails to add anything to the database
     $test=$addScorePrep->bind_param("si", $name, $score);           
     if($test===false)
         die('bind() failed: ' . htmlspecialchars($addScorePrep->error));
-    //  print "entry bind <br /><br />";
-        // var_dump($blogEntryPrep["sqlstate"]);
     
         $test=$addScorePrep->execute();
         if($test===false)
             die('execute() failed: ' . htmlspecialchars($addScorePrep->error));
-        //   else echo "Entry Added!";
 }
 // Having trouble getting the highest scores while keeping the user name distinct. I don't want Poornima flooding the database!
 //https://stackoverflow.com/questions/11434124/mysql-select-distinct-by-highest-value
@@ -33,17 +52,14 @@ if(!is_null($name) && !is_null($score)){
 $getScore="select max(score) as highest, name from highScore_codeBootcamp group by name order by highest desc";
 $result = $mysqli->query($getScore);
 
+// And here we create the JSON to return to our script.
 $scoreArray='[';
 // output data of each row
 while($row = $result->fetch_assoc()) {
     $scoreArray.='{"user":"'.$row["name"].'","score":"'.$row["highest"].'"},';
 }
  $scoreArray=trim($scoreArray,",");
- //$scoreArray.='{"user":"Ben","score":"20"},';
 $scoreArray.=']';
 echo $scoreArray;
 $mysqli->close();
-
-
-
 ?>
